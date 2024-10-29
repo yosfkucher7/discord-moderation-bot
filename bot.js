73e275a55cb248e060552c95fdb222bf8aa73a0f
Hoshiko :(-!!🧙 <60076818+Hoshikoooo@users.noreply.github.com>
2024-10-29 09:47:06 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,13 @@ require("dotenv").config();
 const fs = require("fs");
 const { Client, Collection, Intents } = require("discord.js");
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS
+    ]
+});
 client.commands = new Collection();
 
 // Load commands from the 'commands' folder
